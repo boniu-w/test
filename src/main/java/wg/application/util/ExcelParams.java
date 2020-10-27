fffda2099e8a08d93068f3ce37bbf7723095c44d
wanggang <wanggang@yxkj-tj.com>
2020-10-27 18:13:03 +0800

    modify: 修改了导入, 导出 现在是可以真正当成工具用了

---
+++ src/main/java/wg/application/util/ExcelParams.java
@@ -0,0 +1,23 @@
+package wg.application.util;
+
+import lombok.Data;
+
+/*************************************************************
+ * @Package wg.application.util
+ * @author wg
+ * @date 2020/10/27 16:26
+ * @version
+ * @Copyright
+ *************************************************************/
+@Data
+public class ExcelParams {
+    
+    private int sheetIndex;
+    private int titleIndex;
+    private int contentIndex;
+
+    private String exportFileName;
+    private String exportSheetName;
+
+
+}
--- src/main/java/wg/application/util/ExcelUtil.java
+++ src/main/java/wg/application/util/ExcelUtil.java
@@ -1,24 +1,33 @@
 package wg.application.util;
 
+import com.alibaba.druid.util.StringUtils;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.formula.functions.T;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import wg.application.entity.BankFlow;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Field;
+import java.sql.Timestamp;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /*************************************************************
@@ -26,7 +35,7 @@ import java.util.*;
  * @author wg
  * @date 2020/7/2 10:00
  * @version
- * @Copyright
+ * @Copyright 使用本工具 要结合 我的 Excel 注解使用,
  *************************************************************/
 @Slf4j
 public class ExcelUtil {
@@ -66,7 +75,7 @@ public class ExcelUtil {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            System.out.println("<><><><><>< 初始化 workbook 完成 ><><><><>");
+            //System.out.println("<><><><><>< 初始化 workbook 完成 ><><><><>");
         }
 
         return new HSSFWorkbook();
@@ -78,7 +87,7 @@ public class ExcelUtil {
      * @author: wg
      * @time: 2020/4/26 11:38
      ***************************************************/
-    public static List<MultipartFile> getFileInfo(HttpServletRequest request) {
+    public static List<MultipartFile> getUploadFiles(HttpServletRequest request) {
         // (这里使用Vector，而不使用ArrayLsit，是怕引起线程安全问题，因为后面会引用到相同的内存地址)
         List<MultipartFile> fileVector = new Vector<>();
 
@@ -120,7 +129,7 @@ public class ExcelUtil {
      * @author: wg
      * @time: 2020/7/2 15:05
      ****************************************************************/
-    public static String[] readExcelTitle(int sheetIndex, Class<T> tClass) {
+    public static String[] readExcelTitle(ExcelParams excelParams, Class<T> tClass) {
         if (workbook == null) {
             try {
                 throw new Exception("Workbook对象为空！");
@@ -129,7 +138,10 @@ public class ExcelUtil {
             }
         }
 
-        sheet = workbook.getSheetAt(sheetIndex);
+        int numberOfSheets = workbook.getNumberOfSheets();
+        //System.out.println("numberOfSheets: "+numberOfSheets);
+
+        sheet = workbook.getSheetAt(excelParams.getSheetIndex());
         row = sheet.getRow(0);
         // 标题总列数
         int colNum = row.getPhysicalNumberOfCells();
@@ -146,7 +158,7 @@ public class ExcelUtil {
             for (int i = 0; i < colNum; i++) {
                 cellValue = row.getCell(i).getStringCellValue();
                 for (int j = 0; j < fields.length; j++) {
-                    if (fields[j].isAnnotationPresent(Excel.class)){
+                    if (fields[j].isAnnotationPresent(Excel.class)) {
                         annotation = fields[j].getAnnotation(Excel.class);
                         annotationName = annotation.name();
                         if (annotationName.equals(cellValue)) {
@@ -159,11 +171,7 @@ public class ExcelUtil {
         } catch (NullPointerException nullPointer) {
             nullPointer.printStackTrace();
         }
-
-
         return title;
-
-
     }
 
 
@@ -173,10 +181,10 @@ public class ExcelUtil {
      * @time: 2020/7/2 14:09
      ****************************************************************/
     public static Map<Integer, Map<String, Object>> readExcelContent(Workbook workbook, String[] titleArray,
-                                                                     int sheetIndex) throws Exception {
+                                                                     ExcelParams excelParams) throws Exception {
         Map<Integer, Map<String, Object>> contentMap = new HashMap<Integer, Map<String, Object>>();
         // 得到总行数
-        sheet = workbook.getSheetAt(sheetIndex);
+        sheet = workbook.getSheetAt(excelParams.getSheetIndex());
         int rowNum = sheet.getLastRowNum();
 
         // 总列数
@@ -197,7 +205,7 @@ public class ExcelUtil {
 
         }
 
-        System.out.println(contentMap);
+        //System.out.println(contentMap);
         return contentMap;
     }
 
@@ -249,6 +257,125 @@ public class ExcelUtil {
             cellvalue = "";
         }
         return cellvalue;
+    }
+
+
+    /***************************************************
+     * 导出excel
+     * @author: wg
+     * @time: 2020/4/17 15:01
+     ***************************************************/
+    public static void export(String data, HttpServletResponse response, Class<T> clazz, ExcelParams excelParams) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+        Timestamp timestamp = null;
+        Date date = null;
+        Float aFloat = null;
+        String s = null;
+
+
+        List<T> objectList = new ArrayList<>();
+        if (!StringUtils.isEmpty(data)) {
+            objectList = JSON.parseArray(data, clazz);
+
+        }
+
+        String fileName = excelParams.getExportFileName();
+        String sheetName = excelParams.getExportSheetName();
+
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet(sheetName);
+        HSSFRow row = null;
+        HSSFCell cell = null;
+
+        CellStyle cellStyle = workbook.createCellStyle();
+        cellStyle.setFillForegroundColor(HSSFColor.GOLD.index);
+        cellStyle.setFillPattern(CellStyle.SOLID_FOREGROUND);
+
+        /************ 把所有 带有@Excel 注解的字段拿出来 形成arrayList 以形成表头,并创建单元格写入表头标题 -> 开始 ************/
+        Field[] fields = clazz.getDeclaredFields();
+        ArrayList<Field> fieldArrayList = new ArrayList<>();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(Excel.class)) {
+                fieldArrayList.add(field);
+            }
+        }
+        row = sheet.createRow(0);
+        for (int i = 0; i < fieldArrayList.size(); i++) {
+            cell = row.createCell(i);
+            Field field = fieldArrayList.get(i);
+            Excel annotation = field.getAnnotation(Excel.class);
+            cell.setCellType(Cell.CELL_TYPE_STRING);
+            cell.setCellValue(annotation.name());
+        }
+        /************ 把所有 带有@Excel 注解的字段拿出来 形成arrayList 以形成表头,并创建单元格写入表头标题 -> 结束 ************/
+
+        /************ 导出表的正文 field.get(bankFlow) -> 开始 ************/
+        try {
+            for (int i = 0; i < objectList.size(); i++) {
+                row = sheet.createRow(i + 1);
+                T flow = objectList.get(i);
+                for (int j = 0; j < fieldArrayList.size(); j++) {
+                    cell = row.createCell(j);
+
+                    //if (!StringUtils.isEmpty(flow.getTick()) && flow.getTick().equals("conditionExcel")) {
+                    //    cell.setCellStyle(cellStyle);
+                    //}
+
+                    Field field = fieldArrayList.get(j);
+
+                    //System.out.println("field.toString(): " + field.toString());
+
+                    field.setAccessible(true);
+
+                    if (field.get(flow) instanceof String) {
+                        s = (String) field.get(flow);
+                        cell.setCellValue((String) field.get(flow));
+                        System.out.println(s);
+
+                    }
+
+                    if (field.get(flow) instanceof Timestamp) {
+                        timestamp = (Timestamp) field.get(flow);
+                        date = new Date(timestamp.getTime());
+
+                        String format = dateFormat.format(date);
+                        cell.setCellValue(format);
+                        System.out.println(format);
+                    }
+
+                    if (field.get(flow) instanceof Float) {
+                        aFloat = (Float) field.get(flow);
+                        cell.setCellValue(aFloat.toString());
+                        System.out.println(aFloat.toString());
+                    }
+
+                }
+
+            }
+            /************ 导出表的正文 -> 结束 ************/
+
+
+            /************ 设置响应header -> 开始  ************/
+            fileName = new String(fileName.getBytes(), "ISO8859-1");
+            response.setContentType("application/vnd.ms-excel");
+            response.setCharacterEncoding("utf-8");
+            response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+            response.addHeader("Pargam", "no-cache");
+            response.addHeader("Cache-Control", "no-cache");
+            /************ 设置响应header -> 结束  ************/
+
+
+            /************ 流 -> 开始 ************/
+            OutputStream outputStream = response.getOutputStream();
+            workbook.write(outputStream);
+            outputStream.flush();
+            outputStream.close();
+            /************ 流 -> 结束 ************/
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
