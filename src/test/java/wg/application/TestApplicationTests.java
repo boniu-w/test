dc6b919bb761a17c131bc81e087e4518cb5e93c4
wanggang <wanggang@yxkj-tj.com>
2020-11-16 18:04:06 +0800

    add: add 全半角转换方法

--- src/test/java/wg/application/TestApplicationTests.java
+++ src/test/java/wg/application/TestApplicationTests.java
@@ -230,5 +230,67 @@ public class TestApplicationTests {
     }
 
 
+    /**
+     转全角的方法(SBC case)<br/><br/>
+     全角空格为12288，半角空格为32
+     其他字符半角(33-126)与全角(65281-65374)的对应关系是：均相差65248
+     * @param input 任意字符串
+     * @return 半角字符串
+     *
+     */
+    public static String toSBC(String input)
+    {
+        //半角转全角：
+        char[] c=input.toCharArray();
+        for (int i = 0; i < c.length; i++)
+        {
+            if (c[i]==32)
+            {
+                c[i]=(char)12288;
+                continue;
+            }
+            if (c[i]<127)
+                c[i]=(char)(c[i]+65248);
+        }
+        return new String(c);
+    }
+
+
+    /**
+     转半角的函数(DBC case)<br/><br/>
+     全角空格为12288，半角空格为32
+     其他字符半角(33-126)与全角(65281-65374)的对应关系是：均相差65248
+     * @param input 任意字符串
+     * @return 半角字符串
+     *
+     */
+    public static String toDBC(String input) {
+        char[] c = input.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] == 12288) {
+                //全角空格为12288，半角空格为32
+                c[i] = (char) 32;
+                continue;
+            }
+            if (c[i] > 65280 && c[i] < 65375)
+                //其他字符半角(33-126)与全角(65281-65374)的对应关系是：均相差65248
+                c[i] = (char) (c[i] - 65248);
+        }
+        return new String(c);
+    }
+
+
+    @Test
+    public void testBanJiao(){
+
+        String input = "asd,阿萨,分隔符";
+        String toSBC = toSBC(input);
+
+        System.out.println(toSBC);
+    }
+
+
+
+
 
 }
