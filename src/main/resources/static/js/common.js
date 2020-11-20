4570677d1b4da42aefa594ed5bb0d55f6f38f941
wanggang <wanggang@yxkj-tj.com>
2020-11-20 18:49:42 +0800

    add,modify: bootstrap test

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,9 +1,9 @@
 document.write('<!-----------  公共js和css start----------->');
-document.write('<link rel="stylesheet" type="text/css" href="http://localhost:33333/css/bootstrap.min.css">');
+document.write('<link rel="stylesheet" type="text/css" href="http://localhost:33333/css/bootstrap-431/bootstrap.min.css">');
 document.write('<link rel="stylesheet" type="text/css" href="http://localhost:33333/css/bootstrap-theme.min.css">');
 document.write('<script type="text/javascript" src="http://localhost:33333/js/jquery-3.1.1.js"></script>');
 document.write('<script type="text/javascript" src="http://localhost:33333/js/bootstrap.min.js"></script>');
 document.write('<script type="text/javascript" src="https://unpkg.com/axios/dist/axios.min.js"></script>');
-
+document.write('<script type="text/javascript" src="https://unpkg.com/axios/dist/popper.min.js"></script>');
 
 document.write('<!-----------  公共js和css end----------->');
