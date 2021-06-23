872ee9f88aaa6ff40de5edb0cb31eac4669938ad
wanggang <boniu-w@live.cn>
2021-06-23 10:25:45 +0800

    modify: 1. 修改gitignore; 2. 注释掉base64的一些代码, redmi电脑上加载不出来这些包

--- src/main/java/wg/application/aspect/MyAspect.java
+++ src/main/java/wg/application/aspect/MyAspect.java
@@ -6,8 +6,8 @@ import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+//import sun.misc.BASE64Decoder;
+//import sun.misc.BASE64Encoder;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -33,59 +33,59 @@ public class MyAspect {
      * @time: 2020/4/28 13:16
      *************************************************************/
     //@Before("deciphering()")
-    public Object encryptUserName(ProceedingJoinPoint joinPoint) {
-        BASE64Encoder encoder = new BASE64Encoder();
-        String encode = "";
-        try {
-            Object arg = joinPoint.getArgs()[0];
-            Object proceed = joinPoint.proceed();
-
-            if (arg instanceof String) {
-                encode = encoder.encode(((String) arg).getBytes());
-                System.out.println("encode: " + encode);
-                arg = encode;
-            }
-            return arg;
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
-        }
-        return null;
-    }
+//    public Object encryptUserName(ProceedingJoinPoint joinPoint) {
+//        BASE64Encoder encoder = new BASE64Encoder();
+//        String encode = "";
+//        try {
+//            Object arg = joinPoint.getArgs()[0];
+//            Object proceed = joinPoint.proceed();
+//
+//            if (arg instanceof String) {
+//                encode = encoder.encode(((String) arg).getBytes());
+//                System.out.println("encode: " + encode);
+//                arg = encode;
+//            }
+//            return arg;
+//        } catch (Throwable throwable) {
+//            throwable.printStackTrace();
+//        }
+//        return null;
+//    }
 
     //@After("deciphering()")
-    public void dobefore(JoinPoint joinPoint) {
-        BASE64Encoder encoder = new BASE64Encoder();
-
-
-        String arg = (String) joinPoint.getArgs()[0];
-
-        arg = encoder.encode(arg.getBytes());
-        System.out.println("encode: " + arg);
-
-
-    }
+//    public void dobefore(JoinPoint joinPoint) {
+//        BASE64Encoder encoder = new BASE64Encoder();
+//
+//
+//        String arg = (String) joinPoint.getArgs()[0];
+//
+//        arg = encoder.encode(arg.getBytes());
+//        System.out.println("encode: " + arg);
+//
+//
+//    }
 
     /*************************************************************
      *  解密
      * @author: wg
      * @time: 2020/4/28 13:09
      *************************************************************/
-    @After("deciphering()")
-    public void decipheringUserName(JoinPoint joinPoint) {
-        BASE64Decoder decoder = new BASE64Decoder();
-        Object arg = joinPoint.getArgs()[0];
-        System.out.println("decipheringUserName: " + arg);
-        try {
-            if (arg instanceof String) {
-                byte[] bytes = decoder.decodeBuffer((String) arg);
-                String s = new String(bytes);
-                System.out.println("s: " + s);
-                arg = s;
-            }
-        } catch (IOException io) {
-            io.printStackTrace();
-        }
-    }
+//    @After("deciphering()")
+//    public void decipheringUserName(JoinPoint joinPoint) {
+//        BASE64Decoder decoder = new BASE64Decoder();
+//        Object arg = joinPoint.getArgs()[0];
+//        System.out.println("decipheringUserName: " + arg);
+//        try {
+//            if (arg instanceof String) {
+//                byte[] bytes = decoder.decodeBuffer((String) arg);
+//                String s = new String(bytes);
+//                System.out.println("s: " + s);
+//                arg = s;
+//            }
+//        } catch (IOException io) {
+//            io.printStackTrace();
+//        }
+//    }
 
     @Pointcut("execution(public * wg.application.controller.AopTestController.add(..))")
     public void executeAdd() {
--- src/main/java/wg/application/controller/Base64Util.java
+++ src/main/java/wg/application/controller/Base64Util.java
@@ -3,8 +3,8 @@ package wg.application.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+//import sun.misc.BASE64Decoder;
+//import sun.misc.BASE64Encoder;
 
 import java.io.IOException;
 import java.util.Base64;
@@ -18,27 +18,27 @@ import java.util.Base64;
 @Controller
 public class Base64Util {
 
-    @RequestMapping(value = "/base64Util")
-    @ResponseBody
-    public String decipheringUserName(String userName) {
-        userName = "撒旦发射点";
-        try {
-            // 加密
-            BASE64Encoder encoder = new BASE64Encoder();
-            String encode = encoder.encode(userName.getBytes());
-            System.out.println(encode);
-
-            // 解密
-            BASE64Decoder decoder = new BASE64Decoder();
-
-            byte[] bytes = decoder.decodeBuffer(encode);
-            userName = new String(bytes);
-            System.out.println(userName);
-            return userName;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+//    @RequestMapping(value = "/base64Util")
+//    @ResponseBody
+//    public String decipheringUserName(String userName) {
+//        userName = "撒旦发射点";
+//        try {
+//            // 加密
+//            BASE64Encoder encoder = new BASE64Encoder();
+//            String encode = encoder.encode(userName.getBytes());
+//            System.out.println(encode);
+//
+//            // 解密
+//            BASE64Decoder decoder = new BASE64Decoder();
+//
+//            byte[] bytes = decoder.decodeBuffer(encode);
+//            userName = new String(bytes);
+//            System.out.println(userName);
+//            return userName;
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 }
