192f8a2d354d25a6a553b2cba995586ce1068f64
boniu-w <boniu-w@live.cn>
2021-11-24 11:36:33 +0800

    modify: java 调用 cmd 成功

--- src/main/java/wg/application/cmd/CmdTest.java
+++ src/main/java/wg/application/cmd/CmdTest.java
@@ -1,15 +1,12 @@
 package wg.application.cmd;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 public class CmdTest {
     public static void main(String[] args) {
-        test3();
+        testRuntime();
         // runCmdCommand1();
+        // runCmdCommand2();
     }
 
     public static void runCmdCommand() {
@@ -22,59 +19,40 @@ public class CmdTest {
         }
     }
 
-    public static void runCmdCommand1() {
-        String[] command = new String[10];
-        command[0] = "cmd.exe cd /d:";
-        command[1] = "d:";
-        command[2] = "where /R c: wxwork.exe";
-        ProcessBuilder processBuilder = new ProcessBuilder(command);
+    public static void testProcessBuilder() {
+        String property = System.getProperty("report.dir");
+        String[] command = new String[]{"E:\\pandoc-2.16.1\\pandoc.exe", "test.md", "-o", property + "wg.docx"};
+        File file = new File("E:\\pandoc-2.16.1");
 
-        Map<String, String> environment = processBuilder.environment();
-        environment.forEach((k, v) -> System.out.println(k + "=" + v));
+        // String[] string1 = {"mkdir", "wg-test"};
+        // ProcessBuilder processBuilder1 = new ProcessBuilder(string1);
+        // processBuilder1.directory(file);
+        // processBuilder1.redirectErrorStream(true);
+        // try {
+        //     Process process = processBuilder1.start();
+        //     int i = process.waitFor();
+        // } catch (IOException | InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command(command);
+        processBuilder.directory(file); //设置工作目录
+        processBuilder.redirectErrorStream(true);
+
+        // Map<String, String> environment = processBuilder.environment();
+        // environment.forEach((k, v) -> System.out.println(k + "=" + v));
 
         // List<String> commandList = processBuilder.command();
         // commandList.forEach(System.out::println);
 
-        // ProcessBuilder processBuilder = new ProcessBuilder(command);
-        // processBuilder.directory(new File("C:\\Users\\"));//设置工作目录
-
-        File directory = processBuilder.directory();
-        System.out.println("directory -->> " + directory);
+        // File directory = processBuilder.directory();
+        System.out.println("directory -->> " + processBuilder.directory());
 
         try {
-            processBuilder.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void runCmdCommand2() {
-        List<String> commands = new ArrayList<>();
-        commands.add("cmd.exe");
-        commands.add("cd");
-        commands.add("E:\\pandoc-2.16.1");
-        commands.add("e:");
-        commands.add("pandoc -v");
-
-        ProcessBuilder processBuilder = new ProcessBuilder(commands);
-    }
-
-    /************************************************************************
-     * @description: 成功的测试
-     * @author: wg
-     * @date: 16:47  2021/11/23
-     * @params:
-     * @return:
-     ************************************************************************/
-    public static void test3() {
-        // String command = "ping baidu.com";
-        String command = "cmd /c dir";
-        // command = "pandoc test.md -o wg.docx";
-        String[] commands = new String[]{"pandoc", "test.md", "-o", "wg.docx"};
-        File file = new File("E:\\pandoc-2.16.1");
-        Runtime runtime = Runtime.getRuntime();
-        try {
-            Process process = runtime.exec(command, null, null);
+            Process process = processBuilder.start();
+            int i = process.waitFor();
+            System.out.println(i);
             synchronized (InputStream.class) {
                 InputStream inputStream = process.getInputStream();
                 BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "GBK"));
@@ -84,6 +62,45 @@ public class CmdTest {
                     line = br.readLine();
                 }
                 inputStream.close();
+                System.out.println("---success---");
+            }
+            int i1 = process.waitFor();
+            System.out.println(i1);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void runCmdCommand2() {
+        SecurityManager security = System.getSecurityManager();
+        System.out.println(security);
+    }
+
+    /************************************************************************
+     * @description: 成功的测试
+     * @author: wg
+     * @date: 16:47  2021/11/23
+     * @params:
+     * @return:
+     ************************************************************************/
+    public static void testRuntime() {
+        String pandocDir = "E:\\pandoc-2.16.1\\";
+        String redirectDir = "E:\\pandoc-2.16.1\\test\\";
+        String command = pandocDir + "pandoc test.md -o " + redirectDir + "wg.docx";
+        File pandocFile = new File("E:\\pandoc-2.16.1");
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process process = runtime.exec(command, null, pandocFile);
+            synchronized (InputStream.class) {
+                InputStream inputStream = process.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "GBK"));
+                String line = br.readLine();
+                while (line != null) {
+                    System.out.println(line);
+                    line = br.readLine();
+                }
+                inputStream.close();
+                System.out.println("---success---");
             }
             synchronized (InputStream.class) {
                 InputStream errorStream = process.getErrorStream();
@@ -99,6 +116,5 @@ public class CmdTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
--- src/main/java/wg/application/cmd/Test1.java
+++ src/main/java/wg/application/cmd/Test1.java
@@ -1,7 +1,13 @@
 package wg.application.cmd;
 
+import java.util.Properties;
+
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("hello wg");
+        Properties properties = System.getProperties();
+        properties.forEach((k, v) -> System.out.println(k + "=" + v));
+        String property = System.getProperty("report.dir");
+        System.out.println(property);
+        CmdTest.testProcessBuilder();
     }
 }
