9adb5bdf2b4acd742002d2f87a21a5f2bb89e2ef
wanggang <boniu-w@live.cn>
2023-08-18 14:16:18 +0800

    feat: 程序监控 json

---
+++ src/main/java/wg/application/management/ManagementTest.java
@@ -0,0 +1,38 @@
+package wg.application.management;
+
+import java.lang.management.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/************************************************************************
+ * author: wg
+ * description: ManagementTest 
+ * createTime: 13:36 2023/8/18
+ * updateTime: 13:36 2023/8/18
+ ************************************************************************/
+public class ManagementTest {
+
+    public static void main(String[] args) {
+        OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+        String osName = operatingSystemMXBean.getName();
+        String osVersion = operatingSystemMXBean.getVersion();
+        int processors = operatingSystemMXBean.getAvailableProcessors();
+        System.out.println(String.format("操作系统：%s，版本：%s，处理器：%d 个", osName, osVersion, processors));
+
+        CompilationMXBean compilationMXBean = ManagementFactory.getCompilationMXBean();
+        String compilationMXBeanName = compilationMXBean.getName();
+        System.out.println("编译系统：" + compilationMXBeanName);
+
+        MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
+        MemoryUsage heapMemoryUsage = memoryMXBean.getHeapMemoryUsage();
+        long max = heapMemoryUsage.getMax();
+        long used = heapMemoryUsage.getUsed();
+        System.out.println(String.format("使用内存：%dMB/%dMB", used / 1024 / 1024, max / 1024 / 1024));
+
+        List<GarbageCollectorMXBean> gcMXBeans = ManagementFactory.getGarbageCollectorMXBeans();
+        String gcNames = gcMXBeans.stream()
+                .map(MemoryManagerMXBean::getName)
+                .collect(Collectors.joining(","));
+        System.out.println("垃圾收集器：" + gcNames);
+    }
+}
